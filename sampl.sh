a6a01ef5181f26a385670c2dad2bf793f5c62b96
varunviky7 <varunvignesh7@gmail.com>
2018-07-05 20:32:35 +0530

    jenkinsfile

--- sampl.sh
+++ sampl.sh
@@ -4,6 +4,7 @@ pipeline {
         stage('Build') {
             steps {
                 sh 'echo "Hello World"'
+                sh 'echo "hai"'
                 sh '''
                     echo "Multiline shell steps works too"
                     ls -lah
